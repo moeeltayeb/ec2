20a1e11809f540a47235f6424b67d0d7dab7cf52
Mohamed Eltayeb <altayeb@gmail.com>
2016-12-18 19:04:35 +0400

    adjust the deployment and added example usecase

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+#sudo apt-get install software-properties-common
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt-get update
+#sudo apt-get install ansible
 
-sudo apt-get -y install python-pip
+#sudo apt-get -y install python-pip
 
-ansible-playbook -i ansible-environments/hosts ansible-playbooks/playbook.yml --extra-vars='env='$1' instance_count='$2' instance_type='$3' username='username' password='password'' -vvvv
+ansible-playbook -i ansible-environments/hosts ansible-playbooks/provision.yml --extra-vars='env='$1' instance_count='$2' instance_type='$3' username='username' password='password'' --flush-cache -vv
+
+ansible-playbook -i ansible-environments/hosts ansible-playbooks/playbook.yml
